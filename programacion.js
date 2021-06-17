b45ab9f649eb9cd118f94c34a1a516703b04a131
cristianbarra79 <cristianbarra79@gmail.com>
2021-06-17 20:09:02 -0300

    primer desafio

---
+++ programacion.js
@@ -0,0 +1,5 @@
+// pedir año de nacimiento
+const anio = parseInt(prompt("Ingrese año de nacimiento"));
+console.log("Tu edad este año es de: " + (2021 - anio));
+const edad = 2021 - anio;
+alert("Tu edad este año es de: " + edad);
