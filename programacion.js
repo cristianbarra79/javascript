60d67cba5aa040bdaa97b279a367c8109a783bb5
cristianbarra79 <cristianbarra79@gmail.com>
2021-06-17 20:14:25 -0300

    agregue comentarios

--- programacion.js
+++ programacion.js
@@ -1,5 +1,8 @@
 // pedir año de nacimiento
 const anio = parseInt(prompt("Ingrese año de nacimiento"));
+// salida por consola
 console.log("Tu edad este año es de: " + (2021 - anio));
+// constante para calcular edad
 const edad = 2021 - anio;
+// salida alert con la constante de edad
 alert("Tu edad este año es de: " + edad);
